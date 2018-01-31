ffc04f97d0c27e35611d5146e27c3e0950dcc230
tomazy <tomazy@go2.pl>
2018-01-31 18:59:33 -0500

    Build examples in paralell

--- .scripts/docs-examples.sh
+++ .scripts/docs-examples.sh
@@ -5,10 +5,11 @@ mkdir -p docs/examples
 for i in `ls examples`
 do
   pushd examples/$i
-  npm run build
+  npm run build&
   popd
 
   mkdir -p docs/examples/$i
   cp -r examples/$i/dist/* docs/examples/$i
 done
 
+wait
