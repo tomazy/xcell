9550113b0614beb8d45c9c76bf19166c3217c45c
tomazy <tomazy@go2.pl>
2018-01-30 21:55:26 -0500

    DRYed spreadsheet demo

--- examples/spreadsheet/src/index.js
+++ examples/spreadsheet/src/index.js
@@ -11,31 +11,23 @@ sheet.setCell('A3', '-1')
 sheet.setCell('A4', '4')
 sheet.setCell('A5', '5.2')
 
-sheet.setCell('B1', 'SUM(A1:A7) =')
-sheet.setCell('C1', '=SUM(A1:A7)')
+const samples = [
+  'SUM(A1:A7)',
+  'AVERAGE(A1:A7)',
+  'ROUND(C2;2)',
+  'IF(C3 > C2; "More!"; "Less!")',
+  'MIN(A1:A7)',
+  'MAX(A1:A7)',
+  'COUNT(A1:A7)',
+  'A1 + A2 - A3 / A4',
+]
 
-sheet.setCell('B2', 'AVERAGE(A1:A7) =')
-sheet.setCell('C2', '=AVERAGE(A1:A7)')
+for (let i = 0; i < samples.length; i++) {
+  sheet.setCell(`B${i+1}`, `${samples[i]} —>`)
+  sheet.setCell(`C${i+1}`, `=${samples[i]}`)
+}
 
-sheet.setCell('B3', 'ROUND(C2;2) =')
-sheet.setCell('C3', '=ROUND(C2;2)')
-
-sheet.setCell('B4', 'IF(C3 > C2; "Up!"; "Down!") =')
-sheet.setCell('C4', '=IF(C3 > C2; "Up!"; "Down!")')
-
-sheet.setCell('B5', 'MIN(A1:A7) =')
-sheet.setCell('C5', '=MIN(A1:A7)')
-
-sheet.setCell('B6', 'MAX(A1:A7) =')
-sheet.setCell('C6', '=MAX(A1:A7)')
-
-sheet.setCell('B7', 'COUNT(A1:A7) =')
-sheet.setCell('C7', '=COUNT(A1:A7)')
-
-sheet.setCell('B8', 'A1 + A2 - A3 / A4 =')
-sheet.setCell('C8', '=A1 + A2 - A3 / A4')
-
-const root = spreadsheet(5, 8, sheet)
+const root = spreadsheet(6, samples.length + 2, sheet)
 
 const app = yo`
   <div>
